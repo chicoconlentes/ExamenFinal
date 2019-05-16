86b46195bb595fbde477304d633fa13e97d9eff9
Adán Sebastián Salazar Márquez <A01562006@itesm.mx>
2019-05-16 12:56:56 -0600

    Examen Final

---
+++ ProblemaIII/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProblemaIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            char[] b = new char[nombre.Length];//Lee y convierte nombre a arreglo
+
+            string apellido;
+            Console.WriteLine("Escribe tu apellido");
+            apellido = Console.ReadLine();
+            char[] a = new char[apellido.Length];//Lee y convierte apellido a arreglo
+
+            if (a.Length < 3)
+                Console.WriteLine("apellido invalido");
+            else if (b.Length < 3)
+                Console.WriteLine("nombre invalido"); //Validador de datos
+
+                Console.ReadKey();
+        }
+    }
+}
---
+++ ProblemaIV/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemaIV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+            public static void selectionSort(T[] array)
+            {
+                
+                T = array.Length;
+                for (int i = 0; i < n-1; i++)
+                {
+                    int min = array[i];
+                    int minId = i;
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[j] < min)
+                        {
+                            min = array[j];
+                            minId = j;
+                        }
+                    }
+                    // swapping
+                    T temp = array[i];
+                    array[i] = min;
+                    array[minId] = temp;
+                }
+            }
+          
+        
+
+    }
+}
+
